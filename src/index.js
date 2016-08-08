3d01391e12beeb7bee46f36842929ff5c6b24ee3
lab3 <lab3@tcslab.com>
2016-08-08 14:11:28 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 /* eslint-disable no-console */
 console.log('hi');
+
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './search_bar';
+
+/*const App = function(){
+	return <div>Hi!</div>;
+}*/
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+			);
+	}
+
+}
+
+ReactDOM.render(<App />,document.getElementById('app'));
