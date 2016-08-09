5c70c923f360b08f99bc4b38ce1bd0ef5303dbf4
lab3 <lab3@tcslab.com>
2016-08-09 11:35:05 +0530

    state one

--- src/search_bar.js
+++ src/search_bar.js
@@ -2,20 +2,28 @@ import React,{Component} from 'react';
 
 class SearchBar extends Component {
 
+	constructor(props){
+		super(props);
+		this.onInputChange = this.onInputChange.bind(this);
+		this.state = { term : ''};
+	}
 
+	onInputChange(event) {
+		//console.log(event.target.value);
+		this.setState({term: event.target.value});
+	}
 
 	render() {
 		return (
 			<div>
 				<input placeholder="Enter First Name" onChange={this.onInputChange} />
+				Value of the input : {this.state.term}
 			</div>
 
 			);
 	}
 
-	onInputChange(event) {
-		console.log(event.target.value);
-	}
+	
 }
 
 export default SearchBar;
