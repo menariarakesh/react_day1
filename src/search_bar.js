fab6eab5410f3ed464f2818bab4598858476bd1e
lab3 <lab3@tcslab.com>
2016-08-09 11:26:19 +0530

    Event handling

--- src/search_bar.js
+++ src/search_bar.js
@@ -1,13 +1,20 @@
 import React,{Component} from 'react';
 
 class SearchBar extends Component {
+
+
+
 	render() {
 		return (
 			<div>
-				<input />
+				<input placeholder="Enter First Name" onChange={this.onInputChange} />
 			</div>
 
 			);
+	}
+
+	onInputChange(event) {
+		console.log(event.target.value);
 	}
 }
 
